7633f46ab3bcdea2566736f8170703f635c9c928
codron95 <rohan.stride@gmail.com>
2016-07-17 16:29:51 +0530

    Update gpio_module.py

--- gpio_module.py
+++ gpio_module.py
@@ -51,3 +51,33 @@ def digitalReadChange(pin,callback,freq=1,mode=GPIO.BOARD):
 	thread=threading.Thread(target=state_change_callback,args=(pin,callback,freq,mode,))
 	thread.daemon=True
 	thread.start()
+	
+class intthread(threading):
+	def __init__(self,pin,control,filename="log.txt",freq=1,mode=GPIO.BOARD):
+		self.pin=pin
+		self.control=control
+		self.filename=filename
+		self.freq=freq
+		self.mode=mode
+		
+	def file_log():
+		if(pin==None):
+			print("Please specify the pin no.")
+			return
+		else:
+			GPIO.setmode(mode)
+			GPIO.setup(pin,GPIO.IN)
+			file=open(filename,"w+")
+			while control:
+				state=GPIO.input(pin)
+				cur_time=time.asctime(time.localtime(time.time()))
+				log=cur_time+"\t\t"+str(state)+"\n"
+				print(log)
+				file.write(log)
+				time.sleep(freq)
+				
+	def stop():
+		control=0
+		print("exiting thread")
+		
+		
